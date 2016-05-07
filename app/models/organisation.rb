698705d802ff6c005ebdc7592e0a57f203c5b474
Travis Eubanks <tmeubanks113@gmail.com>
2016-05-07 14:07:07 -0500

    need to make the roo  gem work uploading csv and excel

--- app/models/organisation.rb
+++ app/models/organisation.rb
@@ -1,18 +1,14 @@
 class Organisation < ActiveRecord::Base
+require 'roo'
 
     def self.import(file)
-        CSV.foreach(file.path, headers: true) do |row|
-            Organisation.create! row.to_hash
-        end
-        # spreadsheet = open_spreadsheet(file)
-        # header = spreadsheet.row(1)
+        spreadsheet = open_spreadsheet(file)
+        header = spreadsheet.row(1)
+    end
 
-        # (2..spreadsheet.last_row).each do |i|
-        #     row = Hash[[header, spreadsheet.row(i)].transpose]
-        #     organisation = find_by_id(row["id"]) || new
-        #     organisation.attributes = row.to_hash.slice(*accessible_attributes)
-        #     organisation.save!
-        # end
+
+    def self.open_spreadsheet(file)
+        my_csv = Roo::Spreadsheet.open(file.path, extension: :csv)
     end
 
 end
